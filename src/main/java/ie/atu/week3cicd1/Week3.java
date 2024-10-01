41547693dff2b220282b5a708e5a26e3bf5d6e0b
G00399850@atu.ie <G00399850@atu.ie>
2024-10-01 11:13:38 +0100

    Testing PathVariable and RequestParam

--- src/main/java/ie/atu/week3cicd1/Week3.java
+++ src/main/java/ie/atu/week3cicd1/Week3.java
@@ -2,17 +2,24 @@ package ie.atu.week3cicd1;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class Week3 {
-  @GetMapping("/message")
-    public String getMessage()
+  @GetMapping("/hello")
+    public String getHello()
     {
-        return "Some message";
+        return "hello";
     }
-    @GetMapping("/details")
-  public String getDetails()
+    @GetMapping("/greet/{name}")
+  public String getGreetByName(@PathVariable String name)
     {
-      return "Some other message";
+      return "Hello" +name;
+    }
+    @GetMapping("/{details}")
+  public String getDetails(@RequestParam String name, String details)
+    {
+      return "Name and Age" +name +details;
     }
 }
